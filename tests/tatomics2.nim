24ba3eb97cd58568e6412976bd9c4b559c61785d
cabboose <shayanftw@gmail.com>
2024-08-13 10:38:07 +0800

    test fails - need constructor and store for atomics

--- tests/tatomics2.nim
+++ tests/tatomics2.nim
@@ -5,10 +5,11 @@ import ../src/hyalos/atomics2
 suite "Double Word Atomics":
   block:
     ## Use double word atomic fetch add on a int128
-    
-    var count = hint128(hi: 0'u, lo: 1'u)
+
+    var count: HyAtomic[hint128]
+    count.store(hint128(hi:1'u,lo:1'u))
     checkpoint "Created int128 var " % [ $count ]
 
-    let res = atomicAddFetch(count, hint128(hi: 1'u))
+    let res = addFetch(count, hint128(hi: 1'u))
     checkpoint "Performed doubleword add fetch"
     check res == hint128(hi: 1'u, lo: 1'u)
